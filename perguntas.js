42d7b2addd7bcbb6db1d9477b87e7b887db20e5f
sarah <sarah.bispo@escola.pr.gov.br>
2025-08-19 14:31:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a raiz 121?',
+    'A raiz é 11'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Russia?',
+    'a capital da Russia é Moscou'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a segunda guerra mundial?',
+    'Ocorreu em 1939 a 1945'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Em inglẽs como se pergunta como vocẽ está?',
+    'Se diz (how are you)'
+)
